166df7c98c2e63e987649b14206780c2fa918d8b
Ilya Kazakov <98tltmylive@gmail.com>
2020-03-13 11:57:37 +0400

    Implemented Navigation Component

--- app/src/main/java/com/readyfo/yaclone/ui/activities/MainActivity.kt
+++ app/src/main/java/com/readyfo/yaclone/ui/activities/MainActivity.kt
@@ -1,87 +1,24 @@
 package com.readyfo.yaclone.ui.activities
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.readyfo.yaclone.ui.fragments.HistoryFragment
-import com.readyfo.yaclone.ui.fragments.TranslatorFragment
-
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.readyfo.yaclone.R
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navView: BottomNavigationView
-    private val fm = supportFragmentManager
-    private var transFrag = TranslatorFragment.newInstance()
-    private var historyFrag = HistoryFragment.newInstance()
-    private var active = transFrag
-
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            com.readyfo.yaclone.R.id.navigation_home -> {
-                removeFragment("history")
-                showFragment("translator")
-                return@OnNavigationItemSelectedListener true
-            }
-            com.readyfo.yaclone.R.id.navigation_dashboard -> {
-                replaceAndHideFragment(historyFrag, "history")
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.readyfo.yaclone.R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            addFragment(transFrag, "translator")
-        }
-
         navView = findViewById(com.readyfo.yaclone.R.id.nav_view)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-    }
 
-    override fun onBackPressed() {
-        if (transFrag.isHidden) {
-            removeFragment("history")
-            showFragment("translator")
-            navView.menu.getItem(0).isChecked = true
-        }
-        else
-            super.onBackPressed()
-    }
+        val navController = findNavController(R.id.nav_host_fragment)
 
-
-    private fun addFragment(fragment: Fragment, tag: String){
-        fm.beginTransaction()
-            .add(com.readyfo.yaclone.R.id.mainFragmentContainer, fragment, tag)
-            .commitAllowingStateLoss()
-    }
-
-    private fun replaceAndHideFragment(fragment: Fragment, tag: String){
-        fm.beginTransaction()
-            .hide(active)
-            .add(com.readyfo.yaclone.R.id.mainFragmentContainer, fragment, tag)
-            .commitAllowingStateLoss()
-    }
-
-    private fun showFragment(tag: String){
-        val fragment = fm.findFragmentByTag(tag)
-        if (fragment != null) {
-            fm.beginTransaction()
-                .show(fragment)
-                .commit()
-        }
-    }
-
-    private fun removeFragment(tag: String){
-        val fragment = fm.findFragmentByTag(tag)
-        if (fragment != null) {
-            fm.beginTransaction()
-                .remove(fragment)
-                .commit()
-        }
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/readyfo/yaclone/ui/fragments/TranslatorFragment.kt
+++ app/src/main/java/com/readyfo/yaclone/ui/fragments/TranslatorFragment.kt
@@ -1,24 +1,13 @@
 package com.readyfo.yaclone.ui.fragments
 
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.readyfo.yaclone.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class TranslatorFragment : Fragment() {
 
     companion object {
@@ -39,6 +28,4 @@ class TranslatorFragment : Fragment() {
 
         super.onViewCreated(view, savedInstanceState)
     }
-
-
 }
